de2f3d2f657c2a52549298b0ac62f7d6d0d3b279
adol88 <103913231+adol88@users.noreply.github.com>
2023-03-24 12:52:22 -0700

    Update MacSyntax.py

--- MacSyntax.py
+++ MacSyntax.py
@@ -1,76 +1,56 @@
 #!/usr/bin/env python3
 
-# Checks if the mac length is valid
+characters = "abcdef0123456789.-:"
+char_list = [char for char in characters]
 
-class CharLen:
-    
-    def __init__(self, mac):
-
-        self.mac = mac
-
-    def length_check(self):
-
-        valid_lengths = [12, 14, 17]
-
-        if len(self.mac) in valid_lengths:
-            mac = CharacterValidate(self.mac)
-            mac = mac.validate()
-        else:
-            print('\n', self.mac, 'is not the correct length')
-
-# Checks if the mac has valid characters
+# Checks the mac-address's length and if it has valid characters.
 class CharacterValidate():
-
-    characters = "abcdef0123456789.-:"
-    char_list = [char for char in characters]    # Lazy way of turning 'characters' into a list
 
     def __init__(self, mac):
 
         self.mac = mac
 
     def validate(self):
-        # Need to rewrite the validate function to incorporate the other mac lengths
+
         if len(self.mac) == 17:
-            for c in self.mac:
-                if CharacterValidate.char_list.count(c):
-                    flag = True
-                else:
-                    flag = False
-                    break
-            self.mac = self.mac, 'colon_dash'
+
+            mac_comp = [c for c in self.mac if char_list.count(c) == 1]
+
+            if ''.join(mac_comp) == self.mac:
+                mac = CharacterPosition(self.mac)
+                mac.hyphen_hex_place()
+            else:
+                print(self.mac, 'has invalid characters')
 
         elif len(self.mac) == 14:
-            print(self.mac)         # Placeholder
+            mac_comp = [c for c in self.mac if char_list[0:17].count(c) == 1]
+            
+            if ''.join(mac_comp) == self.mac:
+                mac = CharacterPosition(self.mac)
+                mac.cisco_place()
+            else:
+                print(self.mac, 'has invalid characters')
+
+        elif len(self.mac) == 13:
+            mac_comp = [c for c in self.mac if char_list[0:18].count(c) == 1]
+
+            if ''.join(mac_comp) == self.mac:
+                mac = CharacterPosition(self.mac)
+                mac.hp_place()
+            else:
+                print(self.mac, 'has invalid characters')
 
         elif len(self.mac) == 12:
-            for c in self.mac:
-                if CharacterValidate.char_list[0:16].count(c):
-                    flag = True
-                else:
-                    flag = False
-                    break
-            self.mac = self.mac, 'string'
 
-        if flag:
-            mac = MacType(self.mac)
-            mac = mac.mac_type()
+            mac_comp = [c for c in self.mac if char_list[0:16].count(c) == 1]
+
+            if ''.join(mac_comp) == self.mac:
+                mac = CharacterPosition(self.mac)
+                mac.string_place()
+            else:
+                print(self.mac, 'has invalid characters')
         else:
-            print('Invalid characters found in:', self.mac[0])
-
-# Determines the type of mac-address
-class MacType:
-        
-    def __init__(self, mac):
-
-        self.mac = mac
-        
-    def mac_type(self):
-        print('Determining mac-type...', self.mac[0]) # Remove this line when done
-        mac = CharacterPosition(self.mac[0])
-        if self.mac[1] == 'string':
-            mac.string_place()
-        elif self.mac[1] == 'colon_dash':
-            mac.colon_hex_place()
+            print('Incorrect Length: ', self.mac)
         
 # Verifies that valid characters are in the correct position in the string
 class CharacterPosition():
@@ -79,33 +59,14 @@ class CharacterPosition():
 
         self.mac = mac
 
-    def string_place(self):
-
-        for c in self.mac:
-            if c in CharacterValidate.characters[0:16]:
-                flag = True
-            else:
-                flag = False
-                break
-        if flag == True:
-            print('Valid MAC-Address: ', self.mac)
-        else:
-            print('Invalid MAC-Address: ', self.mac)
-
-    def hp_place(self):
-        pass
-
-    def cisco_place(self):
-        pass
-
-    def colon_hex_place(self):
+    def hyphen_hex_place(self):
 
         colon_dash_position = [2,5,8,11,14]
         colon = [pos for pos in colon_dash_position if self.mac[pos] == ":"]
         dash = [pos for pos in colon_dash_position if self.mac[pos] == "-"]
 
         if colon == colon_dash_position:
-            flag = True
+            flag = True     # Can probably remove this
             self.mac = self.mac, 'colon'
         elif dash == colon_dash_position:
             flag = True
@@ -113,8 +74,27 @@ class CharacterPosition():
         else:
             flag = False
             print('Invalid mac-address:', self.mac)
-        # This should now be ready to pass into MacPrinter
+
         print(self.mac)
 
-    def hyphen_hex_place(self):
-        pass
+    def cisco_place(self):
+
+        decimal_position = [4, 9]
+        decimal = [pos for pos in decimal_position if self.mac[pos] == '.']
+
+        if decimal == decimal_position:
+            print('Valid Cisco mac address', self.mac)
+        else:
+            print('Invalid Cisco mac address', self.mac)
+
+    def hp_place(self):
+        
+        hyphen_position = 6
+        if self.mac[hyphen_position] == '-':
+            print('Valid HP mac address', self.mac)
+        else:
+            print('Invalid HP mac address', self.mac)
+
+    def string_place(self):
+
+        print(self.mac)
